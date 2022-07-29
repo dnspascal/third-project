769313faeac670c64775f87cb6c2ed3509d002d3
dnspascal <102286019+dnspascal@users.noreply.github.com>
2022-07-29 10:31:36 +0300

    I changed the color of the buttons and the alignment

--- lib/main.dart
+++ lib/main.dart
@@ -21,132 +21,174 @@ class QuizApp extends StatelessWidget {
               style: TextStyle(
                   height: 3,
                   fontWeight: FontWeight.bold,
+                  fontFamily: "Quicksand",
                   fontSize: 26,
                   fontStyle: FontStyle.italic),
             ),
+            SizedBox(height: 3),
             Text("What is your favourite programming language",
                 style: TextStyle(height: 3, fontSize: 18)),
+            SizedBox(
+              height: 50,
+            ),
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                TextButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side: BorderSide(
-                              color: Color.fromARGB(255, 1, 33, 58),
-                            ))),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    "JAVA",
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: Color.fromARGB(255, 1, 33, 58),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          Color.fromARGB(255, 1, 33, 58),
+                        ),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(
+                                      color: Color.fromARGB(255, 1, 33, 58),
+                                    ))),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        "JAVA",
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side: BorderSide(
-                              color: Color.fromARGB(255, 1, 33, 58),
-                            ))),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    "PYTHON",
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: Color.fromARGB(255, 1, 33, 58),
+                    SizedBox(
+                      width: 10,
                     ),
-                  ),
+                    SizedBox(height: 10),
+                    TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          Color.fromARGB(255, 1, 33, 58),
+                        ),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(
+                                      color: Color.fromARGB(255, 1, 33, 58),
+                                    ))),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        "PYTHON",
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          Color.fromARGB(255, 1, 33, 58),
+                        ),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(
+                                      color: Color.fromARGB(255, 1, 33, 58),
+                                    ))),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        "JAVASCRIPT",
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
                 SizedBox(
-                  height: 10,
+                  height: 30,
                 ),
-                TextButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side: BorderSide(
-                              color: Color.fromARGB(255, 1, 33, 58),
-                            ))),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    "JAVASCRIPT",
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: Color.fromARGB(255, 1, 33, 58),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          Color.fromARGB(255, 1, 33, 58),
+                        ),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(
+                                      color: Color.fromARGB(255, 1, 33, 58),
+                                    ))),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        "DART",
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side: BorderSide(
-                              color: Color.fromARGB(255, 1, 33, 58),
-                            ))),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    "DART",
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: Color.fromARGB(255, 1, 33, 58),
+                    SizedBox(
+                      width: 10,
                     ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side: BorderSide(
-                              color: Color.fromARGB(255, 1, 33, 58),
-                            ))),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    "C++",
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: Color.fromARGB(255, 1, 33, 58),
+                    TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          Color.fromARGB(255, 1, 33, 58),
+                        ),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(
+                                      color: Color.fromARGB(255, 1, 33, 58),
+                                    ))),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        "C++",
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side: BorderSide(
-                              color: Color.fromARGB(255, 1, 33, 58),
-                            ))),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    "C",
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: Color.fromARGB(255, 1, 33, 58),
+                    SizedBox(width: 10),
+                    TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          Color.fromARGB(255, 1, 33, 58),
+                        ),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(
+                                      color: Color.fromARGB(255, 1, 33, 58),
+                                    ))),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        "C",
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
               ],
             ),
